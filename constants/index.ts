754188126cf8b09ba8bd61eff7c6a674db001fdf
Sainath Kommagoni <jsreactdevsai@gmail.com>
2023-07-20 16:50:44 +0530

    project initial commit with intro page code

---
+++ constants/index.ts
@@ -0,0 +1,163 @@
+import {
+	mobile,
+	backend,
+	creator,
+	web,
+	javascript,
+	typescript,
+	html,
+	css,
+	reactjs,
+	redux,
+	tailwind,
+	nodejs,
+	mongodb,
+	git,
+	figma,
+	docker,
+	meta,
+	starbucks,
+	tesla,
+	shopify,
+	carrent,
+	jobit,
+	tripguide,
+	threejs,
+} from "../public/assets";
+
+export const navLinks = [
+	{
+		id: "about",
+		title: "About",
+	},
+	{
+		id: "work",
+		title: "Work",
+	},
+	{
+		id: "contact",
+		title: "Contact",
+	},
+];
+
+const services = [
+	{
+		title: "Web Developer",
+		icon: web,
+	},
+	{
+		title: "Backend Developer",
+		icon: backend,
+	},
+];
+
+const technologies = [
+	{
+		name: "HTML 5",
+		icon: html,
+	},
+	{
+		name: "CSS 3",
+		icon: css,
+	},
+	{
+		name: "JavaScript",
+		icon: javascript,
+	},
+	{
+		name: "TypeScript",
+		icon: typescript,
+	},
+	{
+		name: "React JS",
+		icon: reactjs,
+	},
+	{
+		name: "Redux Toolkit",
+		icon: redux,
+	},
+	{
+		name: "Tailwind CSS",
+		icon: tailwind,
+	},
+	{
+		name: "Node JS",
+		icon: nodejs,
+	},
+	{
+		name: "MongoDB",
+		icon: mongodb,
+	},
+	{
+		name: "Three JS",
+		icon: threejs,
+	},
+	{
+		name: "git",
+		icon: git,
+	},
+	{
+		name: "figma",
+		icon: figma,
+	},
+	{
+		name: "docker",
+		icon: docker,
+	},
+];
+
+const experiences = [
+	{
+		title: "React.js Developer",
+		company_name: "Root Logics LLP",
+		icon: starbucks,
+		iconBg: "#383E56",
+		date: "August 2021 - July 2022",
+		points: [
+			"Developing and maintaining web applications using React.js and other related technologies.",
+			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+			"Implementing responsive design and ensuring cross-browser compatibility.",
+			"Participating in code reviews and providing constructive feedback to other developers.",
+		],
+	},
+
+	{
+		title: "Full stack Developer",
+		company_name: "Free lancing",
+		icon: meta,
+		iconBg: "#E6DEDD",
+		date: "August 2023 - Present",
+		points: [
+			"Developing and maintaining web applications using React.js and other related technologies.",
+			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+			"Implementing responsive design and ensuring cross-browser compatibility.",
+			"Participating in code reviews and providing constructive feedback to other developers.",
+		],
+	},
+];
+
+const projects = [
+	{
+		name: "Managing Tasks",
+		description:
+			"Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+		tags: [
+			{
+				name: "NextJS",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "TypeScript",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+		],
+		image: carrent,
+		source_code_link: "https://github.com/sain-fullstack-developer",
+	},
+];
+
+export { services, technologies, experiences, projects };
